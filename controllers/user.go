a20780ef0b0c44d9c119094453910dfd4ee7827c
pmf <faizpraditya7@gmail.com>
2024-01-22 14:54:30 +0700

    controllers get user

--- controllers/user.go
+++ controllers/user.go
@@ -1,1 +1,51 @@
 package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"go-mongo-crud/models"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type UserController struct {
+	session *mgo.Session
+}
+
+func NewUserController(s *mgo.Session) *UserController {
+	return &UserController{s}
+}
+
+// struict method
+func (uc UserController) GetUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	id := p.ByName("id")
+
+	if !bson.IsObjectIdHex(id) {
+		// w=response, writeheader=tosend header (err code, status code, etc)
+		w.WriteHeader(http.StatusNotFound)
+	}
+
+	oid := bson.ObjectIdHex(id)
+
+	u := models.User{}
+
+	if err := uc.session.DB("dbname").C("collectionname/user").FindId(oid).One(&u); err != nil {
+		w.WriteHeader(404)
+		return
+	}
+
+	uj, err := json.Marshal(u)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "%s\n", uj)
+}
+
+// CreateUser
+// DeleteUser
